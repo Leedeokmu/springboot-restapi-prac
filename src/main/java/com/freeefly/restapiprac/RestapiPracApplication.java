fabeabe3768895e799a53279c5d1fca3e5181f5e
freeefly <freefly3557@gmail.com>
2020-04-13 00:23:24 +0900

    first commit

---
+++ src/main/java/com/freeefly/restapiprac/RestapiPracApplication.java
@@ -0,0 +1,13 @@
+package com.freeefly.restapiprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freeefly/restapiprac/RestapiPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeefly.restapiprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
